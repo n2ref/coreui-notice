262f4b64759fb881686410cfafe2029cf114669f
n2ref <n2ref2@gmail.com>
2024-05-04 13:11:46 +0300

    Обновление версии + дополнение описания

--- src/js/examples.js
+++ src/js/examples.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Simple
     $('#notice-default').click(function () {
-        CoreUI.notice.info("Hello, world!");
+        CoreUI.notice.default("Hello, world!");
     });
 
     $('#notice-warning').click(function () {
@@ -13,6 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
         CoreUI.notice.danger("Hello, world! This is a toast message.");
     });
 
+    $('#notice-info').click(function () {
+        CoreUI.notice.info("Hello, world! This is a toast message.");
+    });
+
+    $('#notice-primary').click(function () {
+        CoreUI.notice.primary("Hello, world! This is a toast message.");
+    });
+
+    $('#notice-secondary').click(function () {
+        CoreUI.notice.secondary("Hello, world! This is a toast message.");
+    });
+
     $('#notice-success').click(function () {
         CoreUI.notice.success("Note that the live region needs to be present in the markup before the toast is generated or updated. If you dynamically generate both at the same time and inject them into the page, they will generally not be announced by assistive technologies.");
     });
@@ -20,34 +32,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // positions
     $('#notice-position-top-left').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { position: 'top-left' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { position: 'top-left' });
     });
     $('#notice-position-top-center').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { position: 'top-center' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { position: 'top-center' });
     });
     $('#notice-position-top-right').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { position: 'top-right' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { position: 'top-right' });
     });
     $('#notice-position-bottom-left').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { position: 'bottom-left' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { position: 'bottom-left' });
     });
     $('#notice-position-bottom-center').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { position: 'bottom-center' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { position: 'bottom-center' });
     });
     $('#notice-position-bottom-right').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { position: 'bottom-right' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { position: 'bottom-right' });
     });
 
 
     // Styles
     $('#notice-custom-colors').click(function () {
         CoreUI.notice.info("Hello, world! This is a toast message.", {
-            messageColor: '#ffffffd9',
+            textColor: '#ffffffd9',
             bgColor: '#712cf9',
         });
     });
     $('#notice-icon-info').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", { icon: 'bi bi-info' });
+        CoreUI.notice.default("Hello, world! This is a toast message.", { icon: 'bi bi-info' });
     });
     $('#notice-icon-check').click(function () {
         CoreUI.notice.success("Hello, world! This is a toast message.", { icon: 'bi bi-check' });
@@ -59,22 +71,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Events
     $('#notice-actions-btn').click(function () {
-        CoreUI.notice.info("Hello, world! This is a toast message.", {
-            actionBtnText: 'Повторить',
-            onAction: function (notice) {
-                notice.hide();
-                CoreUI.notice.success("Hello, world! This is a toast message.");
-            },
-            onClose: function () {
-                console.log('close')
-            },
-            onClosed: function () {
-                console.log('closed')
-            }
+        CoreUI.notice.default("Hello, world! This is a toast message.", {
+            buttons: [
+                {
+                    text: 'Button 1',
+                    onclick: function () {
+                        this.hide();
+                        CoreUI.notice.success("Hello, world! This is a toast message.");
+                    }
+                },
+                {
+                    text: 'Button 2',
+                    onclick: function () {
+                        this.hide();
+                    }
+                }
+            ],
+            onClose: function () { console.log('close') }
         });
     });
     $('#notice-actions-timer').click(function () {
-        let notice = CoreUI.notice.info("Timer: 5", {
+        let notice = CoreUI.notice.default("Timer: 5", {
             timeout: 0
         });
 
